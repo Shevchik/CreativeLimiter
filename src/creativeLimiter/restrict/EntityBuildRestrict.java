7db866b20a52a6c3d5c34e12512e58ed02c06df2
Shevchik <Shev4ik.den@gmail.com>
2014-04-21 10:57:59 +0400

    Block witherboss creation

--- src/creativeLimiter/restrict/EntityBuildRestrict.java
+++ src/creativeLimiter/restrict/EntityBuildRestrict.java
@@ -1,5 +1,9 @@
 package creativeLimiter.restrict;
 
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,7 +13,26 @@ public class EntityBuildRestrict implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onBlockPlace(BlockPlaceEvent event) {
-		
+		if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
+			if (!event.getPlayer().hasPermission("CreativeLimiter.bypass")) {
+				Block b = event.getBlockPlaced();
+		        World world = b.getWorld();
+				//block witherboss creation
+		        if (b.getType() == Material.SKULL) {
+                	if (
+						(world.getBlockAt(b.getX(), b.getY() - 1, b.getZ()).getType() == Material.SOUL_SAND) &&
+						(world.getBlockAt(b.getX(), b.getY() - 2, b.getZ()).getType() == Material.SOUL_SAND) &&
+						(world.getBlockAt(b.getX() + 1, b.getY() - 1, b.getZ()).getType() == Material.SOUL_SAND) &&
+						(world.getBlockAt(b.getX() - 1, b.getY() - 1, b.getZ()).getType() == Material.SOUL_SAND) ||
+						(world.getBlockAt(b.getX(), b.getY() - 1, b.getZ() - 1).getType() == Material.SOUL_SAND) &&
+						(world.getBlockAt(b.getX(), b.getY() - 1, b.getZ() + 1).getType() == Material.SOUL_SAND)
+                    ) {
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+			}
+		}
 	}
 
 }
