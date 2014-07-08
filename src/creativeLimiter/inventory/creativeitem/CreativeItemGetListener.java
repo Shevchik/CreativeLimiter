3fc6e4902a4986724f1d26746264a8921a68df36
Shevchik <Shev4ik.den@gmail.com>
2014-07-08 04:35:01 +0400

    Remove lore marker. Just rest item to the state is should be when getting from the creative inv directly.

--- src/creativeLimiter/inventory/creativeitem/CreativeItemGetListener.java
+++ src/creativeLimiter/inventory/creativeitem/CreativeItemGetListener.java
@@ -1,49 +1,56 @@
 package creativeLimiter.inventory.creativeitem;
 
-import java.util.List;
+import java.util.Map;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public class CreativeItemGetListener implements Listener {
-
-	private SpecialEffectsRemover eremover = new SpecialEffectsRemover();
-	private LoreMarker lmarker = new LoreMarker();
-	private String markkeyword = ChatColor.GOLD + "CreativeLimiter";
 
 	@EventHandler
 	public void onCreativeItemGet(InventoryCreativeEvent e) {
 		if (!e.getWhoClicked().hasPermission("CreativeLimiter.bypass")) {
 			ItemStack item = e.getCursor();
 			if (item != null && item.getType() != Material.AIR) {
-				int currentSum = 0;
-				currentSum += eremover.isEnchantsValid(item);
-				currentSum += eremover.isDisplayNameValid(item);
-				currentSum += eremover.isLoreValid(item);
-				currentSum += eremover.isAttributesValid(item);
-				if (currentSum != eremover.getValidSum()) {
-					e.setCancelled(true);
-					return;
-				}
-				if (!isNotMarked(item)) {
-					lmarker.markItem(e.getWhoClicked().getName(), item, markkeyword);
-				}
+				e.setCursor(generateCleanItem(item));
 			}
 		}
 	}
 
-	private boolean isNotMarked(ItemStack item) {
-		if (item.hasItemMeta() && item.getItemMeta().hasLore()) {
-			List<String> lore = item.getItemMeta().getLore();
-			if (lore.contains(markkeyword)) {
-				return false;
+	public ItemStack generateCleanItem(ItemStack oldItem) {
+		//handle base
+		ItemStack newItem = new ItemStack(oldItem.getType());
+		newItem.setAmount(oldItem.getAmount());
+		newItem.setDurability(oldItem.getDurability());
+		//handle books
+		if (oldItem.getType() == Material.ENCHANTED_BOOK) {
+			//book may contain only 1 enchantment so we just check and apply it if it is valid
+			Map<Enchantment, Integer> enchants = oldItem.getEnchantments();
+			if (enchants.size() > 0) {
+				Enchantment ench = enchants.keySet().iterator().next();
+				if (enchants.get(ench) <= ench.getMaxLevel()) {
+					newItem.addEnchantment(ench, enchants.get(ench));
+				}
 			}
 		}
-		return true;
+		//handle heads
+		if (oldItem.getType() == Material.SKULL_ITEM && oldItem.getDurability() == 3) {
+			if (oldItem.hasItemMeta()) {
+				SkullMeta oldSkullMeta = (SkullMeta) oldItem.getItemMeta();
+				if (oldSkullMeta.hasOwner()) {
+					SkullMeta newSkullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.SKULL_ITEM);
+					newSkullMeta.setOwner(oldSkullMeta.getOwner());
+				}
+			}
+		}
+		//return newly generated item
+		return newItem;
 	}
 
 }
